6e89672bbe112e9484bc6532dd20b07fc57925c4
Alex Gavalas <alex.gavalas.92@gmail.com>
2024-06-26 10:28:23 +0300

    feat: adds some more playwright test cases

--- e2e-playwright/example.spec.ts
+++ e2e-playwright/example.spec.ts
@@ -54,3 +54,42 @@ test("it can take a screenshot of a specific coordinates bound box", async ({
     },
   });
 });
+
+test("it can take a screenshot of a specific element that is bigger than the viewport", async ({
+  page,
+}) => {
+  await page.goto("https://playwright.dev/");
+
+  const main = await page.$("main");
+
+  await expect(page).toHaveScreenshot("main.png", {
+    clip: (await main?.boundingBox()) ?? undefined,
+    fullPage: true,
+  });
+});
+
+test("it can take a screenshot of a sidebar that is bigger than the viewport", async ({
+  page,
+}) => {
+  await page.goto("https://jobs.workable.com/");
+
+  const sidebar = await page.$("[data-ui=jobs-list]");
+
+  await expect(page).toHaveScreenshot("sidebar.png", {
+    clip: (await sidebar?.boundingBox()) ?? undefined,
+    fullPage: true,
+  });
+});
+
+test("it can ignore some diffs", async ({ page }) => {
+  await page.goto("https://jobs.workable.com/");
+
+  const input = page.getByPlaceholder("Search by job title or keyword");
+
+  await input.fill("Software Engineer" + Math.random().toString().slice(2, 5));
+
+  await expect(page).toHaveScreenshot("search-software-engineer.png", {
+    clip: (await input?.boundingBox()) ?? undefined,
+    maxDiffPixelRatio: 0.1,
+  });
+});
